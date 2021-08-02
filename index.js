3b44c3b8fabe88fb32ce2f667bbbf8aef4d2ea48
London Rosemary <london.rosemary@gmail.com>
2021-08-02 01:23:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove(main.querySelector("main"));
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+newHeader.innerHTML = "London is the champion!";
